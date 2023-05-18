ff99d32692a4ca6df77e65d97e842dbfdeb50d30
Gamingdy <christgik12@gmail.com>
2023-05-18 15:56:45 +0200

    add month in script.js

--- js/script.js
+++ js/script.js
@@ -9,13 +9,21 @@ async function afficherDate()
 {
     while(true) 
     {
-        await pause(1000);
-        
-        var actual_time = new Date();
-        var difference = Math.floor((actual_time - started_date)/1000);
+        let actual_time = new Date();
+        let difference = Math.floor((actual_time - started_date)/1000);
 
-        const days = Math.floor(difference/86400);
+        let days = Math.floor(difference/86400);
         difference -= days*86400;
+        let years = 0;
+        if (days >= 365){
+            years = Math.floor(days/365);
+            days = days % 365;
+        }
+        let month = 0
+        if (days >= 30){
+            month = Math.floor(days/30);
+            days = days % 30;
+        }
 
         const hours = Math.floor(difference / 3600) % 24;
         difference -= hours * 3600;
@@ -23,14 +31,18 @@ async function afficherDate()
         const minutes = Math.floor(difference / 60) % 60;
         difference -= minutes * 60;
 
-        var elapsed_time = "";
-        elapsed_time += `${days} days, `;
+        let elapsed_time = "";
+        elapsed_time += `${years} years, `;
+        elapsed_time += `${month} month `;
+        elapsed_time += `${days} days `;
 
         elapsed_time += `${hours} h : `;
 
         elapsed_time += `${minutes} min `;
         elapsed_time += `${difference} sec`;
         document.getElementById('elapsed_time').innerHTML = elapsed_time;
+
+        await pause(1000);
     }
 }
 afficherDate();
